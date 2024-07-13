3e06ff84f3bb3387b3ec42ca51746189dedb8026
Han Lee <jhl7@princeton.edu>
2024-07-13 14:44:47 +0300

    test commit

--- main.py
+++ main.py
@@ -14,3 +14,5 @@ if __name__ == '__main__':
     print_hi('this is a sample script to start our Dashboard code from :)')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# test commit
+
